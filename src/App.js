eb644ae2e62790b030126ee895f6564576310efd
Anna An <arzumanyan.an@gmail.com>
2022-10-22 14:51:33 +0400

    Adding Home page initial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
-
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home/Home";
 function App() {
   return (
     <>
       <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Home.css";
+import HomeImg from "../../img/portfolio-image-sample.png";
+import { FaGithub, FaLinkedin, FaTwitterSquare } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
+
+const Home = () => {
+  return (
+    <section className="homepage__container">
+      <div className="homepage__wrapper">
+        <div className="homepage__columns homepage__text-area">
+          <div className="homapage__divider-line"></div>
+          <h1 className="homepage__intro-title">
+            I'm Anna, <br />a front-end developer.
+          </h1>
+          <div className="homepage__intro-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia,
+            deserunt <br /> vero officia distinctio neque minima quasi quisquam
+            qui voluptatem <br /> iusto corrupti.
+          </div>
+          <button className="homepage__follow-btn">Let's talk</button>
+          <div className="homepage__follow-links">
+            <p className="homepage__follow-title">Follow me</p>
+            <div className="homepage__sns-icons-wrapper">
+              <FaGithub className="homepage-sns-icons" />
+              <FaLinkedin className="homepage-sns-icons" />
+              <FaTwitterSquare className="homepage-sns-icons" />
+              <RiInstagramFill className="homepage-sns-icons" />
+            </div>
+          </div>
+        </div>
+        <div className="homepage__columns homepage__img-area">
+          <img className="homepage__img" src={HomeImg} alt="profile" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
