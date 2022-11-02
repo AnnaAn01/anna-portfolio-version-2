a76bcf25a01b3a0a636e0a633377fe12d3f8df93
Anna An <arzumanyan.an@gmail.com>
2022-11-02 17:17:53 +0400

    About section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./Components/Home/Home";
 import TechStack from "./Components/TechStack/TechStack";
 import Pages from "./pages/pages";
 import Portfolio from "./Components/Portfolio/Portfolio";
+import About from "./Components/About/About";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Home />
       <TechStack />
       <Portfolio />
+      <About />
     </>
   );
 }
---
+++ src/Components/About/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./About.css";
+
+const About = () => {
+  return (
+    <div className="about__container" id="about">
+      About
+    </div>
+  );
+};
+
+export default About;
