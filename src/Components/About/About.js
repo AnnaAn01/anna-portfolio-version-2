15cacb3d13a4dc6e2f220b9063ed5fec3b07fd5e
Anna An <arzumanyan.an@gmail.com>
2022-11-03 14:45:02 +0400

    editing About section

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -10,20 +10,26 @@ const About = () => {
           <div className="about__divider-line"></div>
           <h1 className="about__intro-title">About me</h1>
           <div className="about__intro-description">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum odit
-            dolor, odio suscipit ratione quas, consectetur a qui ad doloremque
-            cumque ducimus tempora adipisci eum, nihil deleniti. Magnam atque
-            consequuntur, accusamus tempora nam eos et mollitia quis
-            voluptatibus id, facere dolorem, similique vel sit iste cupiditate
-            aut. Eveniet voluptatem quibusdam velit nemo asperiores in quos?
-            Suscipit esse cupiditate cumque amet! Voluptatum earum natus in cum
-            qui quam quis, nostrum culpa soluta beatae dolorum! Officia maiores
-            temporibus incidunt aspernatur, aut, ipsa at earum saepe quaerat
-            quibusdam natus minus illo voluptate cumque. Doloribus unde amet
-            exercitationem quos aut similique incidunt, molestiae nam
-            blanditiis! Illo recusandae nulla minus voluptas facere, maxime
-            alias consectetur itaque architecto impedit officia veniam
-            quibusdam, voluptates inventore ab!
+            <p className="about__description-p">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum
+              odit dolor, odio suscipit ratione quas, consectetur a qui ad
+              doloremque cumque ducimus tempora adipisci eum, nihil deleniti.
+            </p>
+            <br />
+            <p className="about__description-p">
+              Magnam atque consequuntur, accusamus tempora nam eos et mollitia
+              quis voluptatibus id, facere dolorem, similique vel sit iste
+              cupiditate aut. Eveniet voluptatem quibusdam velit nemo asperiores
+              in quos? Suscipit esse cupiditate cumque amet! Voluptatum earum
+              natus in cum qui quam quis, nostrum culpa soluta beatae dolorum!
+            </p>
+            <br />
+            <p className="about__description-p">
+              Officia maiores temporibus incidunt aspernatur, aut, ipsa at earum
+              saepe quaerat quibusdam natus minus illo voluptate cumque.
+              Doloribus unde amet exercitationem quos aut similique incidunt,
+              molestiae nam blanditiis!
+            </p>
           </div>
           <button className="about__follow-btn">Let's talk</button>
         </div>
